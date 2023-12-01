7597f65477deff9153745e1384effff8ed7008e2
Leosilva077 <caang.2022119minf24@aluno.ifpi.edu.br>
2023-12-01 16:21:35 -0300

    firebase login

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/firebase_options.dart';
 import 'package:flutter_application_1/routes/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const MaterialApp(
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Routes(),
     ));
+}
