dfa74eacf267b50e46bdc6a89be6162fb23d7673
Leosilva077 <caang.2022119minf24@aluno.ifpi.edu.br>
2023-12-14 19:58:57 -0300

    add imagens em categoria

--- lib/screens/categoria.dart
+++ lib/screens/categoria.dart
@@ -9,6 +9,20 @@ class CategoriaScreen extends StatefulWidget {
 }
 
 class _CategoriaScreenState extends State<CategoriaScreen> {
+  // Adicionando uma lista de caminhos de imagem
+  final List<String> imagePaths = [
+    'asset/icon/bebidas.png',
+    'asset/icon/carnes.png',
+    'asset/icon/cereais_paes.png',
+    'asset/icon/doces.png',
+    'asset/icon/frutas.png',
+    'asset/icon/graos.png',
+    'asset/icon/aticinios.png',
+    'asset/icon/massas.png',
+    'asset/icon/oleos_gorduras.png',
+    'asset/icon/pescados.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,16 +40,50 @@ class _CategoriaScreenState extends State<CategoriaScreen> {
   }
 
   Widget _getBody() {
-    return const SafeArea(
-        child: Center(
-      child: Column(
-        children: [
-          Text(
-            'Categoria',
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
-          ),
-        ],
+    return SafeArea(
+      child: Center(
+        child: Column(
+          children: List.generate(5, (index) => _buildRow(index)),
+        ),
       ),
-    ));
+    );
+  }
+
+  Widget _buildRow(int rowIndex) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _buildContainer(imagePaths[rowIndex * 2], 'Texto ${rowIndex * 2 + 1}'),
+        const SizedBox(width: 20), // Espaço entre os Containers
+        _buildContainer(
+            imagePaths[rowIndex * 2 + 1], 'Texto ${rowIndex * 2 + 2}'),
+      ],
+    );
+  }
+
+  Widget _buildContainer(String imagePath, String text) {
+    return GestureDetector(
+      onTap: () {
+        print('Container $text clicado');
+      },
+      child: Container(
+        height: 100, // ajuste a altura conforme necessário
+        width: 100, // ajuste a largura conforme necessário
+        margin: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(imagePath),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.w400, color: Colors.white),
+          ),
+        ),
+      ),
+    );
   }
 }
